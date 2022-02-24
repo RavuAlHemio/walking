7619c2aa23a500c5a724e8520ed2f50be4a1fb34
Ondřej Hošek <ondrej.hosek@tuwien.ac.at>
2022-02-24 17:56:34 +0100

    ts: use non-deprecated hash function

--- ts/webpack.config.js
+++ ts/webpack.config.js
@@ -14,6 +14,7 @@ module.exports = [
         output: {
             filename: "walking.js",
             path: path.join(__dirname, "dist"),
+            hashFunction: "sha256",
         },
         plugins: [
             new CleanWebpackPlugin({
